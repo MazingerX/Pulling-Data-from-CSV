ad9154f25c111b74f986f5840ee783a66a11d2a3
mazingerx <lehinostroza@icloud.com>
2019-09-27 13:29:29 -0500

    Arrays was replaced for List, still two errors need to be fixed

--- src/country_population/CsvReader.cs
+++ src/country_population/CsvReader.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace country_population
 {
-    public class CsvReader
+    class CsvReader
     {   
         private string _csvFilePath;
 
@@ -11,20 +13,20 @@ namespace country_population
             this._csvFilePath = csvFilePath;
         }
 
-        public Country[] ReadFirstNCountries(int nCountries)
+        public List<Country> ReadAllCountries()
         {   
-            Country[] countries = new Country[nCountries];
+            List<Country> countries = new List<Country>();
             
             using(StreamReader sr = new StreamReader(_csvFilePath))
             {
                 // read header line
                 sr.ReadLine();
 
-                for(int i = 0; i < nCountries; i++)
-                {
                     string csvLine = sr.ReadLine();
-                    countries[i] = ReadCountryFromCsvLine(csvLine);
-                }
+                    while((csvLine = sr.ReadLine()) != null)
+                    {
+                        countries.Add(ReadCountryFromCsvLine(csvLine));
+                    }
             }
             
             return countries;
@@ -32,13 +34,36 @@ namespace country_population
 
         public Country ReadCountryFromCsvLine(string csvLine)
         {
-            string[] parts = csvLine.Split(new char[] {','});
+            string[] parts = csvLine.Split(',');
 
-            string name = parts[0];
-            string code = parts[1];
-            string region = parts[2];
-            int population = int.Parse(parts[3]);
+            string name;
+            string code;
+            string region;
+            string popText;
 
+            switch(parts.Length)
+            {
+                case 4:
+                    name = parts[0];
+                    code = parts[1];
+                    region = parts[2];
+                    popText = parts[3];
+                    break;
+
+                case 5:
+                    name = parts[0] + "," + parts[1];
+                    name = name.Replace("\"", null).Trim();
+                    code = parts[2];
+                    region = parts[3];
+                    popText = parts[4];
+                    break;
+                
+                default:
+                    throw new Exception($"Can't parse country from csvLine: {csvLine}");
+            }
+
+            // TryParse leaves population=0 if can't parse
+            int.TryParse(popText, out int population);
             return new Country(name, code, region, population);
         }
     }
--- src/country_population/Program.cs
+++ src/country_population/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace country_population
 {
@@ -9,12 +10,20 @@ namespace country_population
             string filepath = @"‎⁨/Users/luishinostroza/CSharpPractice/Collections/Country_Population/src/country_population/Pop by Largest Final.csv.";
             CsvReader reader = new CsvReader(filepath);
 
-            Country[] countries = reader.ReadFirstCountries(10);
+            List<Country> countries = reader.ReadAllCountries();
+            Country lilliput = new Country("Lilliput", "LIL", "Somewhere", 2_000_000);
+            int lilliputIndex = countries.FindIndex(countries=>x.Population < 2_000_000);
+            
+            countries.Insert(lilliputIndex, lilliput);   // Insert items in a list
+
+            countries.RemoveAt(lilliputIndex);    // Remove items from a list
 
             foreach(Country country in countries)
             {
                 Console.WriteLine($"{PopulationFormatter.FormatPopulation (country.Population).PadLeft(15)}: {country.Name}");
             }
+
+            Console.WriteLine($"{countries.Count} countries");
 
         }
     }
