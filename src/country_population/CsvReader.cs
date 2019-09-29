6008f84b53803782354f1b7c9c72b19721f7c53c
mazingerx <lehinostroza@icloud.com>
2019-09-28 21:10:42 -0500

    This version uses collection of collections. Dictionary and Jogged arrays

--- src/country_population/CsvReader.cs
+++ src/country_population/CsvReader.cs
@@ -11,73 +11,69 @@ namespace country_population
     {   
         private string _csvFilePath;
 
-        public CsvReader (string csvFilePath)
-        {
-            this._csvFilePath = csvFilePath;
-        }
+		public CsvReader(string csvFilePath)
+		{
+			this._csvFilePath = csvFilePath;
+		}
 
-        public List<Country> ReadAllCountries()
-        {   
-            var countries = new List<Country>();
-            
-            using(StreamReader sr = new StreamReader(_csvFilePath))
-            {
-                // read header line
-                sr.ReadLine();
+		public Dictionary<string, List<Country>> ReadAllCountries()
+		{
+			var countries = new Dictionary<string, List<Country>>();
 
-                    string csvLine = sr.ReadLine();
-                    while((csvLine = sr.ReadLine()) != null)
-                    {
-                        Country country = ReadCountryFromCsvLine(csvLine);
-                        countries.Add(country);
-                    }
-            }
-            
-            return countries;
-        }
+			using (StreamReader sr = new StreamReader(_csvFilePath))
+			{
+				// read header line
+				sr.ReadLine();
 
-        public void RemoveCommaCountries(List<Country> countries)
-        {
-            for (int i = 0; i < countries.Count; i++)
-            {
-                if (countries[i].Name.Contains(','))
-                countries.RemoveAt(i);
-            }
-        }
+				string csvLine;
+				while ((csvLine = sr.ReadLine()) != null)
+				{
+					Country country = ReadCountryFromCsvLine(csvLine);
+					if (countries.ContainsKey(country.Region))
+					{
+						countries[country.Region].Add(country);
+					}
+					else
+					{
+						List<Country> countriesInRegion = new List<Country>() { country };
+						countries.Add(country.Region, countriesInRegion);
+					}
+				}
+			}
 
-        public Country ReadCountryFromCsvLine(string csvLine)
-        {
-            string[] parts = csvLine.Split(',');
+			return countries;
+		}
 
-            string name;
-            string code;
-            string region;
-            string popText;
+		public Country ReadCountryFromCsvLine(string csvLine)
+		{
+			string[] parts = csvLine.Split(',');
+			string name;
+			string code;
+			string region;
+			string popText;
+			switch (parts.Length)
+			{
+				case 4:
+					name = parts[0];
+					code = parts[1];
+					region = parts[2];
+					popText = parts[3];
+					break;
+				case 5:
+					name = parts[0] + ", " + parts[1];
+					name = name.Replace("\"", null).Trim();
+					code = parts[2];
+					region = parts[3];
+					popText = parts[4];
+					break;
+				default:
+					throw new Exception($"Can't parse country from csvLine: {csvLine}");
+			}
 
-            switch(parts.Length)
-            {
-                case 4:
-                    name = parts[0];
-                    code = parts[1];
-                    region = parts[2];
-                    popText = parts[3];
-                    break;
+			// TryParse leaves population=0 if can't parse
+			int.TryParse(popText, out int population);
 
-                case 5:
-                    name = parts[0] + "," + parts[1];
-                    name = name.Replace("\"", null).Trim();
-                    code = parts[2];
-                    region = parts[3];
-                    popText = parts[4];
-                    break;
-                
-                default:
-                    throw new Exception($"Can't parse country from csvLine: {csvLine}");
-            }
-
-            // TryParse leaves population=0 if can't parse
-            int.TryParse(popText, out int population);
-            return new Country(name, code, region, population);
-        }
+			return new Country(name, code, region, population);
+		}
     }
 }
--- src/country_population/Program.cs
+++ src/country_population/Program.cs
@@ -13,32 +13,22 @@ namespace country_population
 			string filePath = @"‎⁨‎⁨//Users/luishinostroza/CSharpPractice/Collection/Pop by Largest Final.csv";
 			CsvReader reader = new CsvReader(filePath);
 
-			List<Country> countries = reader.ReadAllCountries();
+			Dictionary<string, List<Country>> countries = reader.ReadAllCountries();
 
-			// comment this out to see all countries, without removing the ones with commas
-			reader.RemoveCommaCountries(countries);
+			foreach (string region in countries.Keys)
+				Console.WriteLine(region);
 
-			Console.Write("Enter no. of countries to display> ");
-			bool inputIsInt = int.TryParse(Console.ReadLine(), out int userInput);
-			if (!inputIsInt || userInput <= 0)
+			Console.Write("Which of the above regions do you want? ");
+			string chosenRegion = Console.ReadLine();
+
+			if (countries.ContainsKey(chosenRegion))
 			{
-				Console.WriteLine("You must type in a +ve integer. Exiting");
-				return;
+				// display 10 highest population countries in the selected region
+				foreach (Country country in countries[chosenRegion].Take(10))
+					Console.WriteLine($"{PopulationFormatter.FormatPopulation (country.Population).PadLeft(15)}: {country.Name}");
 			}
-
-			int maxToDisplay = userInput;
-			for (int i = 0; i< countries.Count; i++)
-			{
-				if (i > 0 && (i % maxToDisplay == 0))
-				{
-					Console.WriteLine("Hit return to continue, anything else to quit>");
-					if (Console.ReadLine() != "")
-						break;
-				}
-
-				Country country = countries[i];
-				Console.WriteLine($"{i + 1}: {PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
-			}
+			else
+				Console.WriteLine("That is not a valid region");
 		}
 	}
 }
